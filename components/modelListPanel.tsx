a9557d7eb3a3baf72af015bb29a42313b6bac541
rCampbell00 <reecebell@live.co.uk>
2024-07-28 09:18:20 +0100

    thumbnails from server

--- components/modelListPanel.tsx
+++ components/modelListPanel.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import Image from 'next/image';
+import { useEffect, useState, useCallback } from "react";
+import getThumbImage from '@/actions/getThumbImage';
 
 type ModelListPanelProps = {
     id: number;
@@ -10,18 +12,40 @@ type ModelListPanelProps = {
 
 export default function ModelListPanel({id, description, thumbnail, onClick}: ModelListPanelProps) {
 
+    const [savedThumb, setThumbs] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
+    const [isError, setIsError] = useState(false);
+
+    const getThumbnails = useCallback(
+        async () => {
+            setIsLoading(true);
+            setIsError(false);
+            const thumbSaved = await getThumbImage(id);
+            setThumbs(thumbSaved)
+            setIsLoading(false);
+        }, []
+    );
+
+    useEffect(() => {
+        getThumbnails();
+        return;
+      }, [getThumbnails]);
+
     const handleClick = () => {
         onClick(id);
     }
-
+    
     return (
         <div className='items-center  bg-neutral-300 p-2 rounded mb-2 ml-1 mt-2 mr-1' onClick={handleClick}>
+            {savedThumb?
             <Image
-                src={"/static/images"+thumbnail}
-                alt={"Location Thumbnail"}
-                width={640}
-                height={640}
-            />
+            src={"/static/images"+thumbnail}
+            alt={"Location Thumbnail"}
+            width={640}
+            height={640}
+            />:<></>
+            }
+
             <p>{description}</p>
         </div>
     )
